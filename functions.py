1b76cc6d2ea266a84b77fd5be0ab09fc25e12ca9
Amethyst Barnes <abarnes3@gradcenter.cuny.edu>
2024-08-14 15:41:37 -0400

    Added a new line

--- functions.py
+++ functions.py
@@ -2,22 +2,39 @@
 # Replace the "raise NotImplementedError" line with the correct code!
    
 def print_the_instructors_name():
-    raise NotImplementedError
+    print('Dax Feliz')
+print_the_instructors_name()
 
 def print_the_number_of_this_room():
-    raise NotImplementedError
+    print('5382 (I think)')
+print_the_number_of_this_room()
 
-def split_this_string_at_each_space(input_string):
-    raise NotImplementedError
+#def split_this_string_at_each_space(input_string):
+    #raise NotImplementedError
 
 def take_the_average_of_these_numbers(a, b):
-    raise NotImplementedError
+    import numpy as np
+    a = 4
+    b = 8
+    average = (a+b)/2
+    print(average)
+take_the_average_of_these_numbers(a,b)
 
-def print_the_time_now_using_astropy():
-    raise NotImplementedError
+#def print_the_time_now_using_astropy():
+    #raise NotImplementedError
 
-def return_the_minimum_of_two_numbers(a, b):
-    raise NotImplementedError
+#def return_the_minimum_of_two_numbers(a, b):
+    #import numpy as np
+    #a = 4
+    #b = 8
+    #minimum = np.min(a,b)
+    #print(minimum)
+#return_the_minimum_of_two_numbers()
 
-def return_the_std_of_two_numbers(a, b):
-    raise NotImplementedError
+#def return_the_std_of_two_numbers(a, b):
+    #import numpy as np
+    #a = 4
+    #b = 8
+    #stdev = np.stdev(a,b)
+    #print(stdev)
+#return_the_std_of_two_numbers()
